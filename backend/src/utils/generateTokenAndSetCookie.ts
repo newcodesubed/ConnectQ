10e631953cb1e6663c29ca3099d6190b9d2beb50
newcodesubed <subedshahxi@gmail.com>
2025-09-01 22:44:32 +0545

    feat: added [200~import jwt from jsonwebtoken;

---
+++ backend/src/utils/generateTokenAndSetCookie.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+
+export const generateTokenAndSetCookie = (res: Response, userId: string) => {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET as string, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
