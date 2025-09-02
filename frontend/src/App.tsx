2ee701d4739e22a90f216e38191de334ff24ea37
newcodesubed <subedshahxi@gmail.com>
2025-09-02 13:28:38 +0545

    feat: added route in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import RoleSelect from "./pages/RoleSelect";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import VerifyEmail from "./pages/VerifyEmail";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Dashboard from "./pages/Dashboard";
+import Protected from "./components/Protected";
+import { useEffect } from "react";
+import { useAuth } from "./store/auth.store";
 
 export default function App() {
+  const { checkAuth } = useAuth();
+
+  useEffect(() => {
+    checkAuth(); // try to restore session on app load
+  }, []);
+
   return (
-    <div className="bg-amber-300 text-5xl">Hello world</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/role" replace />} />
+        <Route path="/role" element={<RoleSelect />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Protected>
+              <Dashboard />
+            </Protected>
+          }
+        />
+        <Route path="*" element={<div className="p-6">Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
